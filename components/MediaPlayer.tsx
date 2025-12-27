c6e70d8874aa8e002063e9b44ae9b763374cbbf3
Kyle Domingo <kyle.domingo@bunch.capital>
2025-12-27 23:55:30 +0100

    hide download

--- components/MediaPlayer.tsx
+++ components/MediaPlayer.tsx
@@ -236,12 +236,12 @@ export default function MediaPlayer({
           </select>
         </div>
         <div>
-          <button
-            onClick={() => handleDownload()}
-            className={`bg-green-600 text-white hover:bg-green-700 cursor-pointer rounded-lg px-4 py-2 text-sm font-medium transition-colors  focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 focus:ring-offset-gray-900`}
-          >
-            {!isDownloading ? "Download" : "Back"}
-          </button>
+          {/*<button*/}
+          {/*  onClick={() => handleDownload()}*/}
+          {/*  className={`bg-green-600 text-white hover:bg-green-700 cursor-pointer rounded-lg px-4 py-2 text-sm font-medium transition-colors  focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 focus:ring-offset-gray-900`}*/}
+          {/*>*/}
+          {/*  {!isDownloading ? "Download" : "Back"}*/}
+          {/*</button>*/}
         </div>
       </div>
       Disclaimer: Pop-up ads are triggered by these providers. To improve your
